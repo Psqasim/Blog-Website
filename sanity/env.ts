25269f9ea200d0432e13ab91e3dd79607751e902
Psqasim <muhammadqasim0326@gmail.com>
2024-12-28 10:28:21 +0500

    fix

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,20 +1,12 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-12-26'
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-12-26'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||  "pvx62i8z",
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'pvx62i8z'
 
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage)
-  }
+export const useCdn = false
 
-  return v
-}
+// This makes sure the values are set
+export const token = process.env.SANITY_API_TOKEN
+
+// Log configuration during initialization (remove in production)
